d521fbd39be4aa854b9eefa4295acf102ce8f8c1
shakazed <shakazed83@gmail.com>
2020-02-27 17:05:45 +0100

    Added dooder random walk

---
+++ Assets/Scripts/AI/Dooder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Dooder : MonoBehaviour {
+    private Vector3 _direction;
+    [SerializeField]
+    private float _speed = 10;
+
+    private void Start() {
+        InvokeRepeating("ChangeMoveDir", 5, 5);
+    }
+
+    private void ChangeMoveDir() {
+        _direction = UnityEngine.Random.insideUnitCircle * _speed;
+        _direction.y = 0;
+    }
+
+    private void Update() {
+        Move();
+    }
+
+    private void Move() {
+        transform.position += _direction * Time.deltaTime;
+    }
+}
