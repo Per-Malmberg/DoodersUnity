33eb0d2b221ca0452e7ae295d46234b4e627e259
Per Malmberg <shakazed83@gmail.com>
2020-03-17 13:00:26 +0100

    Added sight.

---
+++ Assets/Scripts/Environment/FoodSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Environment {
+    public class FoodSpawner : MonoBehaviour {
+        [SerializeField] private int _min, _max;
+        [SerializeField] private GameObject _food;
+
+        private void Start() {
+            Invoke("SpawnFood", UnityEngine.Random.Range(_min, _max));
+        }
+
+        private void SpawnFood() {
+            Instantiate(_food, new Vector3(UnityEngine.Random.Range(-50, 50), 0, UnityEngine.Random.Range(-50, 50)), Quaternion.identity);
+            Invoke("SpawnFood", UnityEngine.Random.Range(_min, _max));
+        }
+    }
+}
---
+++ Assets/Scripts/Senses/Sight.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Environment;
+using UnityEngine;
+
+namespace Senses {
+    public class Sight : MonoBehaviour {
+        internal Food CurrentTarget { get; set; }
+
+        [SerializeField] private float _distance = 50;
+        [SerializeField] private float _viewAngle = 45;
+
+        private void Update() {
+            if (CurrentTarget == null) {
+                return;
+            }
+            var forward = gameObject.transform.forward;
+            var foodz = FindObjectsOfType<Food>();
+            foreach (var food in foodz) {
+                if (Vector3.Distance(gameObject.transform.position, food.transform.position) > _distance) {
+                    continue;
+                }
+                var foodDir = food.transform.position - gameObject.transform.position;
+                if (Vector3.Angle(forward, foodDir) > _viewAngle / 2f) {
+                    continue;
+                }
+                CurrentTarget = food;
+            }
+        }
+    }
+}
